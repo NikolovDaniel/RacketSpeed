10dba4ab2de645889098b78e830f7107cc6b2398
Daniel Nikolov <78300496+NikolovDaniel@users.noreply.github.com>
2023-06-20 16:56:38 +0300

    Changed the design for all News related Pages
    20th June:
    - Changed the design for News/Post Add Page for Administrators.
    - Changed the design for News/Post/Edit/{Id} Page.
    - Additionally made small changes to the design of all News pages.

---
+++ RacketSpeed/RacketSpeed.Infrastructure/Migrations/20230619074128_ChangedPostTitleAndContentLength.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RacketSpeed.Infrastructure.Migrations
+{
+    public partial class ChangedPostTitleAndContentLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Posts",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Posts",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Posts",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Posts",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(3000)",
+                oldMaxLength: 3000);
+        }
+    }
+}
