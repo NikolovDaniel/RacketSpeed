99ad89c77a1b09a0a684dbc2bcde140456431fd9
Daniel Nikolov <78300496+NikolovDaniel@users.noreply.github.com>
2023-07-05 23:24:01 +0300

    New design for Login and Register pages with additional functionalities
    4th July:
    - Front-End changes:
    - Back-End changes:
        - Added Email/Account confirmation functionality, without confirming the account, the user cannot log-in.
    
    5th July:
    - Front-End changes:
        - Created new Login page design.
        - New page for resending the email confirmation.
    - Back-End changes:
        - Added Account controller to manage the resend email confirmation and password recovery.
        - Added email confirmation functionality.

--- RacketSpeed/RacketSpeed/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ RacketSpeed/RacketSpeed/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -15,6 +15,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using RacketSpeed.Infrastructure.Data.Entities;
+using RacketSpeed.Infrastructure.Utilities;
 
 namespace RacketSpeed.Areas.Identity.Pages.Account
 {
@@ -42,10 +43,10 @@ namespace RacketSpeed.Areas.Identity.Pages.Account
        
         public class InputModel
         {
-            [Required]
+            [Required(ErrorMessage = DataConstants.RequiredFieldErrorMessage)]
             public string Username { get; set; }
           
-            [Required]
+            [Required(ErrorMessage = DataConstants.RequiredFieldErrorMessage)]
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
@@ -87,7 +88,7 @@ namespace RacketSpeed.Areas.Identity.Pages.Account
                 }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    ModelState.AddModelError(string.Empty, "Грешка при влизане.");
                     return Page();
                 }
             }
---
+++ RacketSpeed/RacketSpeed/Email/SendGridConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace RacketSpeed.Email
+{
+    public class SendGridConfiguration
+    {
+        public string ApiKey { get; set; }
+        public string FromEmail { get; set; }
+        public string FromName { get; set; }
+    }
+}
+
