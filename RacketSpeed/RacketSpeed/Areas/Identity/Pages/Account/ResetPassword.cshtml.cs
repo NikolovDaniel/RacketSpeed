8a88c4f8620084aef24a73dc02a3b6584c8ce72f
Daniel Nikolov <78300496+NikolovDaniel@users.noreply.github.com>
2023-07-06 09:02:31 +0300

    Added Forgot Password functionality and additional new design for Authorisation pages.
    6th July:
    - Front-End changes:
        - Customised the scaffolded pages for Reset Password.
    - Back-End changes:
        - Added scaffolded identity for Reset Password.

---
+++ RacketSpeed/RacketSpeed/Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
@@ -0,0 +1,92 @@
+#nullable disable
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+using RacketSpeed.Infrastructure.Data.Entities;
+using RacketSpeed.Infrastructure.Utilities;
+
+namespace RacketSpeed.Areas.Identity.Pages.Account
+{
+    public class ResetPasswordModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ResetPasswordModel(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required(ErrorMessage = DataConstants.RequiredFieldErrorMessage)]
+            [EmailAddress(ErrorMessage = "Емайла не е валиден.")]
+            public string Email { get; set; }
+
+            [Required(ErrorMessage = DataConstants.RequiredFieldErrorMessage)]
+            [StringLength(100, ErrorMessage = "Паролата трябва да съдържа между 6 и 100 символа.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Потвърди парола")]
+            [Compare("Password", ErrorMessage = "Паролите не съвпадат.")]
+            public string ConfirmPassword { get; set; }
+
+            [Required]
+            public string Code { get; set; }
+
+        }
+
+        public IActionResult OnGet(string code = null)
+        {
+            if (code == null)
+            {
+                return BadRequest("Нужен е токен за да се промени паролата.");
+            }
+            else
+            {
+                Input = new InputModel
+                {
+                    Code = code
+                };
+                return Page();
+            }
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var user = await _userManager.FindByEmailAsync(Input.Email);
+            if (user == null)
+            {
+                return RedirectToPage("./ResetPasswordConfirmation");
+            }
+
+            var result = await _userManager.ResetPasswordAsync(user, Input.Code, Input.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToPage("./ResetPasswordConfirmation");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return Page();
+        }
+    }
+}
--- RacketSpeed/RacketSpeed/Controllers/AccountController.cs
+++ RacketSpeed/RacketSpeed/Controllers/AccountController.cs
@@ -11,30 +11,23 @@ namespace RacketSpeed.Controllers
     public class AccountController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
-        private readonly SignInManager<ApplicationUser> _signInManager;
-        private readonly SendGridConfiguration _sendGridConfiguration;
         private readonly SendGridEmailSender _sendGridEmailSender;
 
         public AccountController(
             UserManager<ApplicationUser> userManager,
-            SignInManager<ApplicationUser> signInManager,
-            IOptions<SendGridConfiguration> optionsAccessor,            SendGridEmailSender sendGridEmailSender)
+            SendGridEmailSender sendGridEmailSender)
         {
             _userManager = userManager;
-            _signInManager = signInManager;
-            _sendGridConfiguration = optionsAccessor.Value;
             _sendGridEmailSender = sendGridEmailSender;
         }
 
         [HttpGet]
-        [AutoValidateAntiforgeryToken]
         public IActionResult ResendConfirmationEmail()
         {
             return View();
         }
 
         [HttpPost]
-        [AutoValidateAntiforgeryToken]
         public async Task<IActionResult> ResendConfirmationEmail(string email)
         {
             if (string.IsNullOrEmpty(email))
@@ -67,41 +60,9 @@ namespace RacketSpeed.Controllers
                  $"Потвърди акаунта си като <a href='{HtmlEncoder.Default.Encode(callBackUrl)}'>кликнеш тук!</a>."
              );
 
-            return RedirectToAction("Login", "Account", new { area = "Identity" });
-        }
-
-        [HttpGet]
-        [ValidateAntiForgeryToken]
-        public IActionResult ForgotPassword()
-        {
+            ViewData["IsResended"] = true;
 
             return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> ForgotPassword(string email)
-        {
-            var user = await _userManager.FindByEmailAsync(email);
-            if (user == null || !await _userManager.IsEmailConfirmedAsync(user))
-            {
-                // Handle the case when user is not found or email is not confirmed
-                return RedirectToAction("ForgotPassword"); // Redirect back to the form
-            }
-
-            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
-
-            var resetLink = Url.Page(
-               "/Account/ResetPassword",
-               pageHandler: null,
-               values: new { userId = user.Id, code = token },
-               protocol: Request.Scheme);
-
-            // Use your SendGridEmailSender to send the password reset email
-            await _sendGridEmailSender.SendEmailAsync(email, "Промени паролата си.",
-                $"Променете паролата си като кликнете върху този линк: <a href='{resetLink}'>линк</a>");
-
-            return RedirectToAction("ResetPassword", "Account", new { area = "Identity" });
         }
     }
 }
